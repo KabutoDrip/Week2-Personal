b74c832125a34c20f992dd54c0711b27d28248b7
Camden Miller <91078110+KabutoDrip@users.noreply.github.com>
2023-05-06 21:31:31 -0600

    LO3 Personal

--- backend/routes/contacts.js
+++ backend/routes/contacts.js
@@ -5,6 +5,15 @@ const contactsController = require('../controllers/contacts');
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/', contactsController.getData);
+router.get('/', contactsController.getAll);
+
+router.get('/:id', contactsController.getSingle);
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/', contactsController.getAll);
+
 // localhost:8080/contacts/
 module.exports = router;
